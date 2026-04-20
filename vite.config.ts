0256d34ffe9c40aea12a4defef935ce61f70f409
Fatima Mohamed <adropofilm@gmail.com>
2026-04-19 20:52:10 -0500

    configure host app with module federation and project scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'host',
+      remotes: {
+        piecesRemote: 'http://localhost:3001/assets/remoteEntry.js',
+        creationsRemote: 'http://localhost:3002/assets/remoteEntry.js',
+        tourGuideRemote: 'http://localhost:3003/assets/remoteEntry.js',
+      },
+      shared: {
+        react: { singleton: true, eager: true },
+        'react-dom': { singleton: true, eager: true },
+        'pixel-retroui': { singleton: true, eager: false },
+        '@mudlabs/ui': { singleton: true, eager: false },
+      },
+    }),
+  ],
+  build: {
+    target: 'esnext',
+  },
 })
